0da00741fbfbefab91b40a489ede23aba68961e6
mmasaid <mohammadmasaed@optimumpartners.co>
2021-09-06 15:19:34 +0300

    Q1- toy a problem

---
+++ toCamelCase/index.test.js
@@ -0,0 +1,23 @@
+/**
+ * Complete the method/function so that it converts dash/underscore delimited words into camel casing.
+ * The first word within the output should be capitalized only if the original word was capitalized (known as Upper Camel Case, also often referred to as Pascal case).
+*/
+
+/**
+ * Examples
+  "the-stealth-warrior" gets converted to "theStealthWarrior"
+  "The_Stealth_Warrior" gets converted to "TheStealthWarrior"
+ */
+
+  function toCamelCase(str){
+    return str.replace(/[_-]\w/gi, ch => ch[1].toUpperCase());
+  }
+
+  describe("Tests", () => {
+    it("test", () => {
+      expect(toCamelCase('')).toStrictEqual('')
+      expect(toCamelCase("the_stealth_warrior")).toStrictEqual("theStealthWarrior")
+      expect(toCamelCase('The-Stealth-Warrior')).toStrictEqual('TheStealthWarrior')
+      expect(toCamelCase('A-B-C')).toStrictEqual('ABC')
+    });
+  });
---
+++ toSnakeCase/index.test.js
@@ -0,0 +1,26 @@
+/**
+ * Complete the method/function so that it converts dash/underscore delimited words or any type from camel casing to snake case
+ * The first word within the output should be capitalized only if the original word was capitalized (known as Upper Camel Case, also often referred to as Pascal case).
+*/
+
+/**
+ * Examples
+  "theStealthWarrior"  gets converted to "the-stealth-warrior"
+  "TheStealthWarrior"  gets converted to "The_Stealth_Warrior"
+  [{ abcD: 1, user: { userId: 2 } }]  gets converted to [{ abc_d: 1, user: { user_id: 2 } }]
+ */
+
+  function toSnakeCase(str){
+    // return to do ...
+  }
+
+  describe("Tests", () => {
+    it("test", () => {
+      expect(toSnakeCase('')).toStrictEqual('')
+      expect(toSnakeCase("theStealthWarrior")).toStrictEqual("the_stealth_warrior")
+      expect(toSnakeCase('TheStealthWarrior')).toStrictEqual('the_stealth_warrior')
+      expect(toSnakeCase('ABC')).toStrictEqual('A_B_C')
+      expect(toSnakeCase({ abcD: 1 })).toStrictEqual({ abc_d: 1 })
+      expect(toSnakeCase([{ abcD: 1, user: {userId: 2} }])).toStrictEqual([{ abc_d: 1, user: {user_id: 2} }])
+    });
+  });
