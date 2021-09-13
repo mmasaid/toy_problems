11410806ac277face3b530d154bcf4ab735a60a0
mmasaid <mohammadmasaed@optimumpartners.co>
2021-09-13 10:27:38 +0300

    closures

---
+++ closures/index.test.js
@@ -0,0 +1,21 @@
+/*
+Write a function pingPongTracker that accepts no arguments and returns an object with the
+following methods:
+- timeSpentPlaying() should return the total amount of time you have played pingpong.
+- playOneGame() should increase the total time you played pingpong by 15 minutes 
+  and return a string "Game played"
+- myLevel() should return your experience level based on how much time you have spent playing
+  pingpong. These are the levels: a) Less than 30 minutes - "I need to improve my game"
+                                  b) 30-100 minutes - "You need to improve your game"
+                                  c) More than 100 minutes - "Wow, I have wasted a lot of time"
+Example:
+var myGame = pingPongTracker();
+myGame.playOneGame();//should return "Game played";
+myGame.playOneGame();//should return "Game played";
+myGame.timeSpentPlaying(); //should return 30;
+myGame.myLevel(); //should return "You need to improve your game"
+*/
+
+function pingPongTracker () {
+// TO DO
+}
