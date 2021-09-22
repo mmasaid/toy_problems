12e18472a80a8670dd0e77e7e20547253058a75d
mmasaid <mohammadmasaed@optimumpartners.co>
2021-09-22 11:00:22 +0300

    add unique

---
+++ unique/index.test.js
@@ -0,0 +1,22 @@
+/**
+ * Given a single input array, write a function that outputs an unique array of values
+ *
+ *
+ * What time complexity is your solution?
+ *
+ * Your solution shouldn't using any build in function.
+ *
+ * Credit: Write a unit tests.
+ */
+
+/**
+  * example usage:
+  * const arrOfNum = [1, 2, 2, 4, 5, 6, 6];
+  * console.log(getUniqueValues(arrOfNum)); // [1, 2, 4, 5, 6]
+  */
+const arrOfNum = [1, 2, 2, 4, 5, 6, 6];
+
+const getUniqueValues = (arrOfNum) => {
+  // TO DO
+}
+console.log(getUniqueValues(arrOfNum)); // [1, 2, 4, 5, 6]
