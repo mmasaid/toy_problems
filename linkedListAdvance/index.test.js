fec697317fb974197bf47d342e207fe1db579dda
mmasaid <mohammadmasaed@optimumpartners.co>
2021-09-26 10:56:12 +0300

    add advance linked list

---
+++ linkedListAdvance/index.test.js
@@ -0,0 +1,26 @@
+/*
+ * Implement a linked list using the pseudoclassical instantiation pattern.
+ *
+ * https://medium.com/@taylorshephard1/instantiation-patterns-in-javascript-7f9463b95839
+ *
+ * Your linked list should have methods called "addToTail", "removeHead", and "contains."
+ *
+ */
+
+// EXAMPLE USAGE:
+// const list = new LinkedList();
+// list.tail;         //yields 'null'
+// list.addToTail(4);
+// list.addToTail(5);
+// list.head.value;   //yields '4';
+// list.contains(5);  //yields 'true';
+// list.contains(6);  //yields 'false';
+// list.removeHead(); //yields '4'
+// list.tail.value;   //yields '5';
+
+
+const LinkedList = () => {
+  //fill me in!
+};
+
+//write methods here!
