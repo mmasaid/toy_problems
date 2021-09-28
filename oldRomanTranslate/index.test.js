e90983a9bdea237174a660a329344df93ce318c0
mmasaid <mohammadmasaed@optimumpartners.co>
2021-09-28 10:55:46 +0300

    add translate roman numeral

---
+++ oldRomanTranslate/index.test.js
@@ -0,0 +1,31 @@
+
+/**
+ * Given a roman numeral as input, write a function that converts the roman
+ * numeral to a number and outputs it.
+ *
+ * Example:
+ * translateRomanNumeral("XXI") // 21
+ *
+ * When a smaller numeral appears before a larger one, it becomes
+ * a subtractive operation. You can assume only one smaller numeral
+ * may appear in front of larger one.
+ *
+ * Example:
+ * translateRomanNumeral("IV") // 4
+ *
+ * You should return `null` on invalid input.
+ */
+
+const DIGIT_VALUES = {
+  I: 1,
+  V: 5,
+  X: 10,
+  L: 50,
+  C: 100,
+  D: 500,
+  M: 1000
+};
+
+ const translateRomanNumeral = (romanNumeral)=> {
+   // TO DO ...
+  }
