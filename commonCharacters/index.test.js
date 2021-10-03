7394c13dda7e065ac7406e3d9380f36b6f588515
mmasaid <mohammadmasaed@optimumpartners.co>
2021-10-03 11:11:40 +0300

    add common character

---
+++ commonCharacters/index.test.js
@@ -0,0 +1,15 @@
+/**
+ * Write a function `commonCharacters(str1, str2)` which takes two strings as arguments and returns a
+ * string containing the characters found in both strings (without duplication), in the
+ * order that they appeared in `str1`. Remember to skip spaces and characters you
+ * have already encountered!
+ *
+ * Example: commonCharacters('acexivou', 'aegihobu')
+ * Returns: 'aeiou'
+ *
+ * Extra credit: Extend your function to handle more than two input strings.
+ */
+
+const commonCharacters = (str1, str2) => {
+  // TODO
+};
