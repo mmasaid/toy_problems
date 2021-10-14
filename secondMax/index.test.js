e433b66bce9a276e704236d5c1c6e87b7abd0951
mmasaid <mohammadmasaed@optimumpartners.co>
2021-10-14 11:07:17 +0300

    secondMax

---
+++ secondMax/index.test.js
@@ -0,0 +1,15 @@
+/*
+You have to create a function
+that takes an array of integer number and returns the
+second max in the array:
+
+Note : using sort native is not allowed
+
+Exanmples:
+secondMax([1,5, 2])== 2
+secondMax([-1, -5, 2])== -1
+*/
+
+const secondMax = function(array) {
+  // TODO
+};
