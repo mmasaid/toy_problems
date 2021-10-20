804a85c0ee727ee985b0b1528065605d90d77d69
mmasaid <mohammadmasaed@optimumpartners.co>
2021-10-20 11:15:23 +0300

    add prime test

---
+++ primeTesting/index.test.js
@@ -0,0 +1,17 @@
+/**
+ * A prime number is an integer number that cannot be divided by any number
+ * except itself and 1.
+ * Write a function that accepts a number and returns true if it's
+ * a prime number, false if it's not.
+ */
+
+const primeTester = function(n) {
+
+};
+
+/* Extra credit: Write a function that generates a list of all prime numbers
+  * in a user-specified range (inclusive).
+ If you're not quite sure where to start,
+  * check out the Sieve of Eratosthenes on Wikipedia. (And if you're feeling
+  * saucy, check out the Sieve of Atkin.)
+*/
