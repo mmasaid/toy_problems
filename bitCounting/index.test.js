3ee8368c5824fb7e8ee59a7cf5479e70c4a77f50
Jad Abu Awwad <jad.abuawwad@optimumpartners.co>
2022-09-27 13:13:19 +0300

    feat : bitCounting problem solved

--- bitCounting/index.test.js
+++ bitCounting/index.test.js
@@ -7,6 +7,17 @@
  * so the function should return 5 in this case
  */
 
-const bitCounting = (num) => {
-  // TODO
-};
+const convertToBinary = (dec) => {
+  if (dec > 0) {
+    return convertToBinary(parseInt(dec / 2)) + (dec % 2)
+  };
+  return ''
+}
+const bitCounting = (number) => {
+  let bin = convertToBinary(number)
+  let count = 0
+  for (const bit of bin) {
+    if (bit == 1) count++
+  }
+  return count
+}
