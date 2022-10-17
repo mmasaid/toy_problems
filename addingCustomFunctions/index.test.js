39a6fe9d4627fe5f0d40adca895d62a0467e7694
emad-almajdalawi <e.almajdalawi@gamil.com>
2022-10-17 16:26:41 +0300

    Fix: Throw Error, more narrowing

--- addingCustomFunctions/index.test.js
+++ addingCustomFunctions/index.test.js
@@ -8,12 +8,14 @@
 
 //  TODO
 
+emptyArrayError = 'Empty Array!'
+
 
 Array.prototype.customSort = function () {
     // https://www.geeksforgeeks.org/insertion-sort/
 
     let n = this.length
-    if (n == 0) return 'Empty Array!'
+    if (n == 0) return Promise.reject(new Error(emptyArrayError))
 
     let i, key, j;
     for (i = 1; i < n; i++) {
@@ -31,28 +33,21 @@ Array.prototype.customSort = function () {
 
 
 Array.prototype.getFirstElement = function () {
-    if (this[0]) return this[0]
-    else return 'Empty Array!'
+    if (!this[0]) return Promise.reject(new Error(emptyArrayError))
+
+    return this[0]
 }
 
 
 Array.prototype.getLastElement = function () {
-    if (!this[0]) return 'Empty Array!'
+    if (!this[0]) return Promise.reject(new Error(emptyArrayError))
 
-    let counter
-    for (let i = 0; ; i++) {
-        if (this[i]) counter = i
-        else break
-    }
-
-    return this[counter]
+    return this[this.length - 1]
 }
 
 
 Array.prototype.search = function (wanted) {
-    // return this.filter((element) => element === wanted)
-
-    if (this.length == 0) return 'Empty Array!'
+    if (this.length == 0) return Promise.reject(new Error(emptyArrayError))
 
     let results = []
     this.forEach((element, index) => {
@@ -64,7 +59,7 @@ Array.prototype.search = function (wanted) {
         }
     })
 
-    if (results.length == 0) return 'Not Found!'
+    if (results.length == 0) return Promise.reject(new Error('Not Found!'))
     else return results
 }
 
@@ -81,38 +76,45 @@ arr3 = []
 arr4 = [true, 1, 'word']
 arr5 = [1, 2, 3, 'word', 4, 2]
 
-describe('Tests', () => {
-    it('test customSort', () => {
-        expect(arr.customSort()).toEqual(
-            [-1, 0, 2, 2, 4, 4, 6, 8, 9, 93]
-        );
+describe('customSort', () => {
+    it('Should sort the array', () => {
+        expect(arr.customSort()).toEqual([-1, 0, 2, 2, 4, 4, 6, 8, 9, 93]);
         expect(arr2.customSort()).toEqual([1]);
-        expect(arr3.customSort()).toEqual('Empty Array!');
     });
-    it('test getFirstElement', () => {
+    it('Should thow error', () => {
+        expect(arr3.customSort()).rejects.toThrow(emptyArrayError);
+    });
+})
+
+describe('getFirstElement', () => {
+    it('Should return the first element of the array', () => {
         expect(arr5.getFirstElement()).toEqual(1);
-        expect(arr3.getFirstElement()).toEqual('Empty Array!');
         expect(arr4.getFirstElement()).toEqual(true);
     })
-    it('test getLastElement', () => {
-        expect(arr.getLastElement()).toEqual(-1);
-        expect(arr3.getLastElement()).toEqual('Empty Array!');
+    it('Should thow error', () => {
+        expect(arr3.getFirstElement()).rejects.toThrow(emptyArrayError);
+    })
+})
+
+describe('getFirstElement', () => {
+    it('Shuld return the last element of the array', () => {
+        expect(arr5.getLastElement()).toEqual(2);
         expect(arr4.getLastElement()).toEqual('word');
     })
-    it('test search', () => {
-        expect(arr5.search(2)).toEqual(
-            [{ index: 1, value: 2 }, { index: 5, value: 2 }]
-        );
-        expect(arr5.search(1)).toEqual(
-            [{ index: 0, value: 1 }]
-        );
-        expect(arr5.search('1')).toEqual('Not Found!');
-        expect(arr5.search('word')).toEqual(
-            [{ index: 3, value: 'word' }]
-        );
-        expect(arr2.search(1)).toEqual(
-            [{ index: 0, value: 1 }]
-        );
-        expect(arr3.search(1)).toEqual('Empty Array!');
+    it('Should thow error', () => {
+        expect(arr3.getLastElement()).rejects.toThrow(emptyArrayError);
+    })
+})
+
+describe('getFirstElement', () => {
+    it('Should return an array of objects with the keys (value, index)', () => {
+        expect(arr5.search(2)).toEqual([{ index: 1, value: 2 }, { index: 5, value: 2 }]);
+        expect(arr5.search(1)).toEqual([{ index: 0, value: 1 }]);
+        expect(arr5.search('word')).toEqual([{ index: 3, value: 'word' }]);
+        expect(arr2.search(1)).toEqual([{ index: 0, value: 1 }]);
+    })
+    it('Should thow error', () => {
+        expect(arr5.search('1')).rejects.toThrow('Not Found!');
+        expect(arr3.search(1)).rejects.toThrow(emptyArrayError);
     })
 })
